1ca0bd9341b8761442e80abf3f0b11f02e120aa3
darthirule <seanbuchas@yahoo.com>
2015-07-18 14:08:33 -0400

    Initial Commit

---
+++ Asteroids/Game1.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Asteroids
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+        ParticleEngine particleEngine;
+        TileGen tile;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.IsFixedTimeStep = true;
+            this.graphics.SynchronizeWithVerticalRetrace = true;
+            this.graphics.PreferredBackBufferHeight = 980;
+            this.graphics.PreferredBackBufferWidth = 1820;
+            this.TargetElapsedTime = new System.TimeSpan(0, 0, 0, 0, 16);
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+
+            tile = new TileGen();
+            tile.Init();
+
+                base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("Font");
+            List<Texture2D> textures = new List<Texture2D>();
+            textures.Add(Content.Load<Texture2D>("circle"));
+            textures.Add(Content.Load<Texture2D>("star"));
+            textures.Add(Content.Load<Texture2D>("diamond"));
+            particleEngine = new ParticleEngine(textures, new Vector2(400, 200));
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (IsActive)
+            {
+
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
+
+                particleEngine.EmitterLocation = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+                particleEngine.Update(gameTime);
+                tile.Update(this.GraphicsDevice);
+
+                base.Update(gameTime);
+            }
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+                                               
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, "Hello World", new Vector2(200, 200), Color.White);
+            spriteBatch.End();
+
+            tile.Draw(spriteBatch);
+
+            particleEngine.Draw(spriteBatch);
+
+            base.Draw(gameTime);
+        }
+
+        protected override void OnActivated(object sender, System.EventArgs args)
+        {
+            this.Window.Title = "Active Application";
+            base.OnActivated(sender, args);
+        }
+
+        protected override void OnDeactivated(object sender, System.EventArgs args)
+        {
+            this.Window.Title = "InActive Application";
+            base.OnDeactivated(sender, args);
+        }
+    }
+}
---
+++ Asteroids/TileGen.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids
+{
+   public class TileGen
+    {
+       
+       private Random random;
+       int sX;
+       int sY;
+       int sH = 60;
+       int sW = 60;
+       public List<Rectangle> Squares;
+       public List<Texture2D> Textures;
+       private Texture2D texture;
+       bool inter = false;
+
+       private Rectangle GenSquare()
+       {
+           sX = (int)random.Next(1920);
+           sY = (int)random.Next(1080);
+
+           Rectangle rect = new Rectangle(sX, sY, sW, sH);
+           
+           return rect;
+       }
+
+       public void Init()
+       {
+           Squares = new List<Rectangle>();
+           Textures = new List<Texture2D>();
+           random = new Random();
+       }
+
+
+       private Texture2D GenTexture(GraphicsDevice graphics)
+       {
+
+           Texture2D Texture;
+
+           Color color = new Color(
+                (float)random.NextDouble(),
+                (float)random.NextDouble(),
+                (float)random.NextDouble());
+
+           Color[] colorData = new Color[100 * 100];
+
+           for (int i = 0; i < 10000; i++)
+               colorData[i] = color;
+
+           Texture = new Texture2D(graphics, sW, sH);
+
+           Texture.SetData<Color>(colorData);
+
+           return Texture;
+
+
+
+       }
+
+
+       public void Update(GraphicsDevice graphics)
+       {
+           for (int i = 0; i <= Squares.Count() - 1; i++)
+           {
+               if (Squares.Count() <= 150)
+               {
+                   if (!inter)
+                   {
+
+                       Squares.Add(GenSquare());
+                   }
+
+                   inter = checkInter(i);
+               }
+           }
+
+           if (Textures.Count() <= 150)
+           {
+               Textures.Add(GenTexture(graphics));
+           }
+
+           texture = GenTexture(graphics);
+
+       }
+
+       public void Draw(SpriteBatch spriteBatch)
+       {
+           spriteBatch.Begin();
+           for (int i = 0; i < Squares.Count(); i++)
+           {
+                   spriteBatch.Draw(Textures[i], Squares[i], Color.White);
+           }
+           spriteBatch.End();
+       }
+
+       private bool checkInter(int Index)
+       {
+
+           bool intersected = false;
+
+           for (int i = 1; i <= Squares.Count() - 1; i++)
+           {
+               if (Squares[Index].Intersects(Squares[i - 1]))
+               {
+                   intersected = true;
+               }
+
+               else intersected = false;
+           }
+
+           return intersected;
+       }
+
+    }
+}
